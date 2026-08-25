b2dcf7f8e838bf8af5f1f2737dcbbb2b4db33ad3
Marco Souza <marco.souza@mongodb.com>
2026-08-25 14:58:36 -0300

    fea: add personal extensions

---
+++ src/rotate-opencode-keys.ts
@@ -0,0 +1,89 @@
+import type { UserMessage } from "@earendil-works/pi-ai";
+import type {
+  ExtensionAPI,
+  ExtensionContext,
+} from "@earendil-works/pi-coding-agent";
+
+const authTokensRaw = Bun.env.OPENCODE_KEYS ?? "";
+const authTokens = authTokensRaw.split(",");
+
+let seqErrorCounter = 0;
+let prevMessageContent: UserMessage["content"] | null = null;
+
+export default function (pi: ExtensionAPI) {
+  pi.on("message_end", async (event, ctx: ExtensionContext) => {
+    if (event.message.role === "user") {
+      prevMessageContent = event.message.content;
+      seqErrorCounter = 0;
+    }
+
+    if (event.message.role !== "assistant") {
+      return;
+    }
+
+    const { stopReason = "", errorMessage = "" } = event.message;
+    if (stopReason !== "error") {
+      seqErrorCounter = 0;
+      return;
+    }
+
+    const isCreditsError = errorMessage.includes("CreditsError");
+    if (!isCreditsError) {
+      seqErrorCounter = 0;
+      return;
+    }
+
+    const nextAuthToken = await rotateOpencodeKeys();
+
+    ctx.ui.notify(
+      `Sessiom Auth Token rotated: ${nextAuthToken.slice(0, 6)}******, errorCounter=${seqErrorCounter}`,
+      "info",
+    );
+
+    // INFO: resend message
+    if (!prevMessageContent) {
+      return;
+    }
+
+    seqErrorCounter++;
+
+    if (seqErrorCounter >= authTokens.length) {
+      ctx.ui.notify(
+        `All tokens rotated and none is working, errorCounter=${seqErrorCounter}`,
+        "warning",
+      );
+      return;
+    }
+
+    pi.sendMessage({
+      customType: "rotate-opencode-keys",
+      content: prevMessageContent,
+      display: true,
+    });
+  });
+}
+
+async function rotateOpencodeKeys() {
+  const file = Bun.file("../auth.json");
+  const {
+    "opencode-go": { key: currentAuthKey },
+  } = await file.json();
+
+  const currentIdx =
+    authTokens.findIndex((token) => token === currentAuthKey) ??
+    raise("couldn't find current auth");
+
+  const nextIdx = (currentIdx + 1) % authTokens.length;
+  const nextAuthToken =
+    authTokens[nextIdx] ?? raise("next token not accessible");
+
+  // Write to auth.json
+  const textContent = await file.text();
+  await file.write(textContent.replaceAll(currentAuthKey, nextAuthToken));
+
+  return nextAuthToken;
+}
+
+function raise(message: string): never {
+  throw new Error(message);
+}
